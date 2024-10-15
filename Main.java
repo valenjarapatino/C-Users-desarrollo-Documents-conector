ced4fe95d8172f30da1ac3fbaf615f47d30da081
valenjarapatino <143661904+valenjarapatino@users.noreply.github.com>
2024-10-15 14:20:06 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.sql.DriverManager;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        connection myConn = null;
+        Statement myStant = null;
+        Resultset myRes = null;
+        try{
+            myConn = DriverManager.getConnection(url: "", user"", password "")
+        }
+
+
+    }
+    }
